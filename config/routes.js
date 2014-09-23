bb1bec1a8bc56f4342ac6f2126200ba8443ef0e6
Robin Ricard <ricard.robin@gmail.com>
2014-09-23 09:15:54 +0200

    Add auth middleware

--- config/routes.js
+++ config/routes.js
@@ -1,10 +1,9 @@
-
+"use strict";
 /**
  * @file Defines the routes available on the server
 
  * Will define all availables exposed HTTP paths, and their methods (GET / POST / ...).
  */
-'use strict';
 var restify = require('restify');
 
 var lib = require('../lib/');
