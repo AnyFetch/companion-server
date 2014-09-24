b276d797c8e29a46a2a4d4466a0005a3d7932763
Robin Ricard <ricard.robin@gmail.com>
2014-09-24 17:47:24 +0200

    No async in documents tests

--- test/documents.js
+++ test/documents.js
@@ -1,6 +1,5 @@
 "use strict";
 
-var async = require('async');
 var request = require('supertest');
 
 var app = require('../app.js');
@@ -34,39 +33,29 @@ describe("Documents endpoint", function() {
     });
 
     it("should accept queries and return results", function(done) {
-      async.waterfall([
-        function queryDocuments(cb) {
-          request(app)
-            .get('/documents?query=test')
-            .set('Authorization', 'Bearer ' + MOCK_SERVER_TOKEN)
-            .expect(200)
-            .end(cb);
-        },
-        function assert(res, cb) {
+      request(app)
+        .get('/documents?query=test')
+        .set('Authorization', 'Bearer ' + MOCK_SERVER_TOKEN)
+        .expect(200)
+        .expect(function assert(res) {
           res.body.should.have.lengthOf(1);
           res.body[0].should.have.property('type', 'file');
           res.body[0].should.have.property('id', '53ce3726f341e34e309ef0bb');
           res.body[0].should.have.property('date', '2014-07-22T10:04:22.441Z');
-          cb();
-        }
-      ], done);
+        })
+        .end(done);
     });
 
     it("should pre-project snippets and title", function(done) {
-      async.waterfall([
-        function queryDocuments(cb) {
-          request(app)
-            .get('/documents?query=test')
-            .set('Authorization', 'Bearer ' + MOCK_SERVER_TOKEN)
-            .expect(200)
-            .end(cb);
-        },
-        function assert(res, cb) {
+      request(app)
+        .get('/documents?query=test')
+        .set('Authorization', 'Bearer ' + MOCK_SERVER_TOKEN)
+        .expect(200)
+        .expect(function assert(res) {
           res.body[0].should.have.property('snippet', "<h1>My Document</h1><code>mydoc.doc</code>");
           res.body[0].should.have.property('title', "My Document");
-          cb();
-        }
-      ], done);
+        })
+        .end(done);
     });
   });
 
@@ -80,38 +69,28 @@ describe("Documents endpoint", function() {
     });
 
     it("should accept access if everything's right", function(done) {
-      async.waterfall([
-        function queryDocuments(cb) {
-          request(app)
-            .get('/documents/53ce3726f341e34e309ef0bb')
-            .set('Authorization', 'Bearer ' + MOCK_SERVER_TOKEN)
-            .expect(200)
-            .end(cb);
-        },
-        function assert(res, cb) {
+      request(app)
+        .get('/documents/53ce3726f341e34e309ef0bb')
+        .set('Authorization', 'Bearer ' + MOCK_SERVER_TOKEN)
+        .expect(200)
+        .expect(function assert(res) {
           res.body.should.have.property('type', "file");
           res.body.should.have.property('id', "53ce3726f341e34e309ef0bb");
           res.body.should.have.property('date', '2014-07-22T10:04:22.441Z');
-          cb();
-        }
-      ], done);
+        })
+        .end(done);
     });
 
     it("should pre-project full and title", function(done) {
-      async.waterfall([
-        function queryDocuments(cb) {
-          request(app)
-            .get('/documents/5252ce4ce4cfcd16f55cfa3b')
-            .set('Authorization', 'Bearer ' + MOCK_SERVER_TOKEN)
-            .expect(200)
-            .end(cb);
-        },
-        function assert(res, cb) {
+      request(app)
+        .get('/documents/5252ce4ce4cfcd16f55cfa3b')
+        .set('Authorization', 'Bearer ' + MOCK_SERVER_TOKEN)
+        .expect(200)
+        .expect(function assert(res) {
           res.body.should.have.property('full', "<h1>My Document</h1><p><code>mydoc.doc</code></p>");
           res.body.should.have.property('title', "My Document");
-          cb();
-        }
-      ], done);
+        })
+        .end(done);
     });
   });
 });
