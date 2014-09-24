ec98241943f681d0ff08ecb6930e9ab3898f03c6
Robin Ricard <ricard.robin@gmail.com>
2014-09-24 14:18:27 +0200

    [ci skip] [red] Tests for /documents

--- test/documents.js
+++ test/documents.js
@@ -1,12 +1,66 @@
 "use strict";
 
+var async = require('async');
+var request = require('supertest');
+
+var app = require('../app.js');
+
+var MOCK_SERVER_TOKEN = "0d7d5dd28e615b2d31cf648df4a5a279e509945b";
+
 describe("Documents endpoint", function() {
+  beforeEach(function createTokenInMongo(done) {
+    request(app)
+      .get('/init/callback?code=test')
+      .expect(302)
+      .end(done);
+  });
+
   describe("GET /documents", function() {
-    it("should refuse access if token is missing");
-    it("should refuse access if query is missing");
-    it("should accept queries and return results");
-    it("should sort documents in a chronological order in time sections");
-    it("should pre-project snippets and title");
+    it("should refuse access if token is missing", function(done) {
+      request(app)
+        .get('/documents?query=test')
+        .expect(403)
+        .end(done);
+    });
+    it("should refuse access if query is missing", function(done) {
+      request(app)
+        .get('/documents')
+        .set('Authentication', 'Bearer ' + MOCK_SERVER_TOKEN)
+        .expect(409)
+        .end(done);
+    });
+    it("should accept queries and return results", function(done) {
+      async.waterfall([
+        function queryDocuments(cb) {
+          request(app)
+            .get('/documents?query=test')
+            .set('Authentication', 'Bearer ' + MOCK_SERVER_TOKEN)
+            .expect(200)
+            .end(cb);
+        },
+        function assert(res, cb) {
+          res.body.should.have.property('length', 1);
+          res.body[0].should.have.propery('type', 'file');
+          cb();
+        }
+      ], done);
+    });
+    it("should pre-project snippets and title", function(done) {
+      async.waterfall([
+        function queryDocuments(cb) {
+          request(app)
+            .get('/documents?query=test')
+            .set('Authentication', 'Bearer ' + MOCK_SERVER_TOKEN)
+            .expect(200)
+            .end(cb);
+        },
+        function assert(res, cb) {
+          res.body[0].should.have.property('snippet', "<h1>My Document</h1>");
+          res.body[0].should.have.property('title', "My Document");
+          cb();
+        }
+      ], done);
+    });
   });
 
   describe("GET /documents/:id", function() {
--- test/index.js
+++ test/index.js
@@ -11,6 +11,90 @@ var MOCK_SERVER_TOKEN = "0d7d5dd28e615b2d31cf648df4a5a279e509945b";
 
 before(function setupServers() {
   var apiServer = Anyfetch.createMockServer();
+  apiServer.override("get", "/documents", {
+    "facets": {
+      "document_types": [
+        {
+          "_type": "DocumentType",
+          "id": "5252ce4ce4cfcd16f55cfa3b",
+          "name": "file",
+          "templates": {
+            "snippet": "<article>\n  <h1>{{{ title }}}</h1>\n  <code>{{{ path }}}</code>\n</article>\n",
+            "full": "<article>\n  <h1>{{{ title }}}</h1>\n  <code>{{{ path }}}</code>\n</article>\n",
+            "title": "{{ title }}"
+          },
+          "description": "Most basic document type for any kind of binary content. When a provider sends data without any additional information, it will use this document_type.",
+          "projections": {
+            "snippet": [
+              "title",
+              "path",
+              "extension"
+            ],
+            "full": [
+              "title",
+              "path",
+              "extension"
+            ]
+          },
+          "document_count": 1
+        }
+      ],
+      "providers": [
+        {
+          "_type": "AccessToken",
+          "id": "53c66af5d3bda13f3a848ab9",
+          "client": null,
+          "is_basic_token": true,
+          "account_name": "",
+          "document_count": 1
+        }
+      ],
+      "creation_dates": [
+        {
+          "_type": "Date",
+          "timestamp": "1404172800000",
+          "date": "Tue Jul 01 2014 02:00:00 GMT+0200 (CEST)",
+          "document_count": 1
+        }
+      ]
+    },
+    "data": [
+      {
+        "_type": "Document",
+        "id": "53ce3726f341e34e309ef0bb",
+        "identifier": "the_unique_identifier",
+        "creation_date": "2014-07-22T10:04:22.441Z",
+        "provider": {
+          "_type": "AccessToken",
+          "id": "53c66af5d3bda13f3a848ab9",
+          "client": null,
+          "is_basic_token": true,
+          "account_name": ""
+        },
+        "company": "52f0bb24c8318c2d65000035",
+        "document_type": {
+          "_type": "DocumentType",
+          "id": "5252ce4ce4cfcd16f55cfa3b",
+          "name": "file",
+          "templates": {
+            "snippet": "<h1>{{{ title }}}</h1><code>{{{ path }}}</code>",
+            "title": "{{{ title }}}"
+          }
+        },
+        "actions": {},
+        "document_url": "/documents/53ce3726f341e34e309ef0bb",
+        "projection_type": "snippet",
+        "data": {
+          "title": "My Document",
+          "path": "mydoc.doc"
+        },
+        "related_count": 0,
+        "score": 1
+      }
+    ],
+    "count": 1,
+    "max_score": 1
+  });
   apiServer.listen(8002);
 
   var managerServer = restify.createServer();
