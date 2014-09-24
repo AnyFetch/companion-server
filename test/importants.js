eb6cb9b03025b470ca85aeb7bd32207f79fcc1f6
Robin Ricard <ricard.robin@gmail.com>
2014-09-24 18:34:22 +0200

    [skip ci] [red] Tests for POST /events/:eventId/importants/:id

--- test/importants.js
+++ test/importants.js
@@ -1,9 +1,13 @@
 "use strict";
 
 var request = require('supertest');
+var async = require('async');
+var mongoose = require ('mongoose');
+var ObjectId = mongoose.Types.ObjectId;
 
 var app = require('../app.js');
 var helpers = require('./helpers.js');
+var ImportantDocument = mongoose.model("ImportantDocument");
 
 function createFakeImportantDocument(done) {
   request(app)
@@ -33,6 +37,40 @@ describe("Important documents endpoint", function() {
           res.body[0].should.have.property('date', '2014-07-22T10:04:22.441Z');
         })
         .end(done);
+    });
+  });
+
+  describe("POST /events/:eventId/importants/:id", function() {
+    it("should refuse access if token is missing", helpers.checkForAuth('post', '/documents/53ce3726f341e34e309ef0bb'));
+
+    it("should add successfully in the database the document", function(done) {
+      async.waterfall([
+        createFakeImportantDocument,
+        function queryMongo(res, cb) {
+          ImportantDocument.findById(new ObjectId("53ce3726f341e34e309ef0bb"), cb);
+        },
+        function assert(document, cb) {
+          document.should.have.property('type', 'file');
+          document.should.have.property('_id', new ObjectId('53ce3726f341e34e309ef0bb'));
+          document.should.have.property('date', '2014-07-22T10:04:22.441Z');
+          document.should.have.property('eventId', 'test');
+          cb();
+        }
+      ], done);
+    });
+
+    it("should complain if the document is already there", function(done) {
+      async.waterfall([
+        createFakeImportantDocument,
+        function createFakeRedundantImportantDocument(cb) {
+          request(app)
+            .post('/events/test/importants/53ce3726f341e34e309ef0bb')
+            .set('Authorization', 'Bearer ' + helpers.MOCK_SERVER_TOKEN)
+            .expect(409)
+            .expect(/document already marked as important/i)
+            .end(cb);
+        }
+      ], done);
     });
   });
 });
