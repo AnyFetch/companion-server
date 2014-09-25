f246a5a4230e7bac39282bb84507e9d300cf0bdb
Robin Ricard <ricard.robin@gmail.com>
2014-09-25 13:05:14 +0200

    Move ObjectId checking as a middleware

--- config/routes.js
+++ config/routes.js
@@ -17,6 +17,6 @@ module.exports = function(server) {
   server.get('/documents/:id', middleware.auth, handlers.documents.id.index.get);
 
   server.get('/events/:eventId/importants', middleware.auth, handlers.events.id.importants.index.get);
-  server.post('/events/:eventId/importants/:id', middleware.auth, handlers.events.id.importants.id.index.post);
-  server.del('/events/:eventId/importants/:id', middleware.auth, handlers.events.id.importants.id.index.del);
+  server.post('/events/:eventId/importants/:id', middleware.auth, middleware.checkObjectId(), handlers.events.id.importants.id.index.post);
+  server.del('/events/:eventId/importants/:id', middleware.auth, middleware.checkObjectId(), handlers.events.id.importants.id.index.del);
 };
