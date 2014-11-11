3b9831ccc57988432c30e25367eb1c6d0e264d87
Neamar <neamar@neamar.fr>
2014-11-11 11:24:44 +0100

    Added logger for requests

--- config/routes.js
+++ config/routes.js
@@ -6,19 +6,19 @@
  */
 var lib = require('../lib/');
 var handlers = lib.handlers;
-var middleware = lib.middleware;
+var middlewares = lib.middlewares;
 
 // Routes client requests to handlers
 module.exports = function(server) {
   server.get('/init/connect', handlers.init.connect.get);
   server.get('/init/callback', handlers.init.callback.get);
 
-  server.get('/start', middleware.auth, handlers.start.index.get);
+  server.get('/start', middlewares.auth, handlers.start.index.get);
 
-  server.get('/documents', middleware.auth, handlers.documents.index.get);
-  server.get('/documents/:id', middleware.auth, handlers.documents.id.index.get);
+  server.get('/documents', middlewares.auth, handlers.documents.index.get);
+  server.get('/documents/:id', middlewares.auth, handlers.documents.id.index.get);
 
-  server.get('/events/:eventId/importants', middleware.auth, handlers.events.id.importants.index.get);
-  server.post('/events/:eventId/importants/:id', middleware.auth, middleware.checkObjectId(), handlers.events.id.importants.id.index.post);
-  server.del('/events/:eventId/importants/:id', middleware.auth, middleware.checkObjectId(), handlers.events.id.importants.id.index.del);
+  server.get('/events/:eventId/importants', middlewares.auth, handlers.events.id.importants.index.get);
+  server.post('/events/:eventId/importants/:id', middlewares.auth, middlewares.checkObjectId(), handlers.events.id.importants.id.index.post);
+  server.del('/events/:eventId/importants/:id', middlewares.auth, middlewares.checkObjectId(), handlers.events.id.importants.id.index.del);
 };
